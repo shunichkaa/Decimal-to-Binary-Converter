bee9d79989af32c56f968a99bf8cfae34bd1c10d
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-29 16:19:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-function decimalToBinary(n) {
-  if (n === 0) return "0";
-  if (n === 1) return "1";
-  return decimalToBinary(Math.floor(n / 2)) + (n % 2);
-}
+function checkUserInput() {
 
-convertBtn.addEventListener("click", () => {
-  const value = Number(numberInput.value);
-  if (Number.isInteger(value) && value >= 0) {
-    result.textContent = decimalToBinary(value);
-  } else {
-    result.textContent = "Please enter a valid positive integer.";
-  }
-});
+}
