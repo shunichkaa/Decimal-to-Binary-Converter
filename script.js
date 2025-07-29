a7d95a48e5ac8548af7b261ffd8fcc65a9a74f40
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-29 16:23:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-function checkUserInput() {
+const checkUserInput = () => {
   console.log(numberInput.value);
-}
+};
+
+convertBtn.addEventListener("click", checkUserInput);
