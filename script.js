9cac59ac8071669c2a8575c7523c00ef3732c2e1
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-30 10:35:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-if (
-  !numberInput.value ||
-  isNaN(parseInt(numberInput.value)) ||
-  parseInt(numberInput.value) < 0
-) {
-}
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+ alert("Please provide a decimal number greater than or equal to 0");
+  return;
+  }
 
   console.log(numberInput.value);
 };
