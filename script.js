41670c2cf36be6ca6fbdacb90c74cefca4a3e314
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-01 11:40:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const decimalToBinary = (input) => {
     const quotient = Math.floor(input / 2);
     const remainder = input % 2;
 
-  inputs.push(input);
+   inputs.push(input);
+  quotients.push(quotient);
+  remainders.push(remainder);
     input = quotient;
   }
 };
