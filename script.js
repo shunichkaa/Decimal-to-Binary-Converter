84dc1c4f5205658c5a74d692f534e03ef8325009
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-06 11:03:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => {
+function decimalToBinary(input) {
   if (input === 0) {
-    return "";
+    return "0";
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-};
+}
 
 const checkUserInput = () => {
   if (
@@ -19,7 +19,6 @@ const checkUserInput = () => {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
-
 
   result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
