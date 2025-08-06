4a8321d030f814f1ace78cb0c9a015f6a96467e8
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-06 11:00:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-if (input === 0) {
+  if (input === 0) {
     return "";
   } else {
-    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
 
