1d06f79015d32eb9ecc49a965d27e4d4635b0c6e
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-06 11:05:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
   if (input === 0) {
     return "0";
-  } 
-  
-  else {
+  } else if (input === 1) {
+    return "1";
+  } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
