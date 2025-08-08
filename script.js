a0cf201678a6bfd2ab3f5baeee70e508e173fb8a
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-08 10:23:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
 
   animationData.forEach((obj) => {
-    setTimeout(() => {
-    animationContainer.innerHTML += ``;
+    setTimeout(() => {    animationContainer.innerHTML += `<p id=""></p>`;
     }, obj.addElDelay);
   });
 };
