1d0bf3db563fee5eb8670959694cf25adfb95917
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-09 09:48:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const animationContainer = document.getElementById("animation-container");
 const animationData = [
   {
     inputVal: 5,
-    addElDelay: 1000
+    addElDelay: 1000,
+    msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+    showMsgDelay:15000,
+    removeElDelay:20000
   },
   {
     inputVal: 2,
     addElDelay: 1500,
-    msg:  "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
     showMsgDelay: 10000,
-    removeElDelay: 15000
+    removeElDelay: 15000,
   },
   {
     inputVal: 1,
